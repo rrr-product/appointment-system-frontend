a10039e2fa5cf90ab14fa386ee27d36b476f3a92
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-04-27 10:49:20 +0530

    Login page is developed

--- appoinment-system/src/app/app.routes.ts
+++ appoinment-system/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    }
+];
---
+++ appoinment-system/src/app/components/login/login.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+  email: string = '';
+  password: string = '';
+
+  onSubmit() {
+    console.log(this.email, this.password);
+  }
+}
