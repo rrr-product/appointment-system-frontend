d658bf56a714f5fb62a5dd422cf60c89bd10b931
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-04-27 10:55:07 +0530

    feat: implement login component with responsive UI and password visibility toggle

--- appoinment-system/src/app/components/login/login.ts
+++ appoinment-system/src/app/components/login/login.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+
+  constructor(private router: Router) { }
+
   email: string = '';
   password: string = '';
+  showPassword = false;
 
   onSubmit() {
     console.log(this.email, this.password);
+    this.router.navigate(['/dashboard']);
   }
 }
